50a9908732999536970356dfd2f6a09e215a122f
Lrunlin <liurl0621@gmail.com>
2025-01-05 22:12:15 +0800

    document

--- intall.bat
+++ intall.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "npm config set registry https://registry.npmmirror.com/&&npm i yarn vite nodemon -g&&yarn config set registry https://registry.npmmirror.com/&&cd server&&yarn&&cd ../admin&&yarn&& exit"
+start cmd /k "npm config set registry https://registry.npmmirror.com/&&npm i yarn vite nodemon cross-env -g&&yarn config set registry https://registry.npmmirror.com/&&cd server&&yarn&&cd ../admin&&yarn&& exit"
